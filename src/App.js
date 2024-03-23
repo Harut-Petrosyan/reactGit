0e6446d43caebbe99385e491e5d2910cf766f3ec
Harut-Petrosyan <angelpetrosyan22@gmail.com>
2024-03-23 10:51:55 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Appi from './Appi';
 import Foot from './Foot';
+import Verst from './Verst';
 
 function App() {
   return (
     <div className="App">
       <Appi/>
+      <Verst/>
       <Foot/>
     </div>
   );
---
+++ src/Verst.js
@@ -0,0 +1,8 @@
+
+
+function Verst() {
+    return <div>
+        <img src="https://miro.medium.com/v2/resize:fit:1400/1*kxBdslclglg4zgCw0NMIIA.png" alt=""></img>
+    </div>
+}
+export default Verst
