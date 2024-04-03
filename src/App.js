d1e2561f2abf54542cb2d2dd60f06544662e04a7
Harut-Petrosyan <angelpetrosyan22@gmail.com>
2024-04-03 22:31:34 +0400

    commaaan

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
+import { useState } from 'react';
 import './App.css';
-import Appi from './Appi';
-import Foot from './Foot';
-import Verst from './Verst';
+import { useEffect } from 'react';
+
 
 function App() {
+  const [title,setTitle]=useState("")
+  const [data,setData]=useState([])
+  useEffect(()=>{
+    if (title.length === 2) {
+    let handle = setTimeout(()=>{
+      fetch("")
+      .then(stream=>stream.json())
+      .then(results=>setData(results.data))
+    },1000)
+    return()=>{
+      clearTimeout(handle)
+    }
+    }
+  },[title])
+  let confirmed = ""
+  let name = ""
+  if (data !== undefined) {
+    confirmed = data.latest_data.confirmed
+    name = data.name
+  }
   return (
     <div className="App">
-      <Appi/>
-      <Verst/>
-      <Foot/>
+      <input 
+      type='text'
+      value={title}
+      onChange={(evt)=>setTimeout(evt.target.value)}
+      />
     </div>
   );
 }
--- src/Verst.js
+++
@@ -1,8 +0,0 @@
-
-
-function Verst() {
-    return <div>
-        <img src="https://miro.medium.com/v2/resize:fit:1400/1*kxBdslclglg4zgCw0NMIIA.png" alt=""></img>
-    </div>
-}
-export default Verst
